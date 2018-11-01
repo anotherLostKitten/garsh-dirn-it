efde3fd20e1eed05c4924c952847ab50852e1808
Theodore Peters <tpeters@stuy.edu>
2018-11-01 16:38:50 -0400

    ....

--- tree.c
+++ tree.c
@@ -6,39 +6,57 @@
 #include <dirent.h>
 #include <string.h>
 
-#define WHITE  "\e[0m"
-#define BLUE  "\x1B[36m"
-
+#define WHITE "\e[0m"
+#define BLUE "\e[96m"
+#define GREEN "\e[92m"
+#define PERMS "-xwrxwrxwr"
 void sizinga(int i,char**p) {
   if(i){
     sizinga(i>>10,p+1);
     if(i&1023)printf("%d %s ",i&1023,*p);
   }
 }
-
-int dirinfo(char* name){
+void dirinfo(char*name,int depth){
   DIR*root=opendir(name);
-  struct dirent* cur;
-  while(cur=readdir(root)){
-    printf("%s%s\n", (cur->d_type&DT_DIR)?BLUE:WHITE, cur->d_name);
+  struct stat*inf=malloc(sizeof(struct stat));
+  int ep=strlen(name);
+  name[ep++]='/';
+  for(struct dirent*cur;cur=readdir(root);){
+    if(*cur->d_name=='.')
+      continue;
+    strcpy(name+ep,cur->d_name);
+    stat(name,inf);
+    if(cur->d_type&DT_DIR){
+      printf("d");
+      for(int i=10;--i;)printf("%c",PERMS[i*(inf->st_mode*2>>i&1)]);
+      printf(" +");
+      for(int i=0;i<=depth;i++)printf("--");
+      printf("%s%s/%s\n",BLUE,cur->d_name,WHITE);
+      dirinfo(name, depth+1);
+    } else {
+      printf("-");
+      for(int i=10;--i;)printf("%c",PERMS[i*(inf->st_mode*2>>i&1)]);
+      printf(" +");
+      for(int i=0;i<=depth;i++)printf("--");
+      printf("%s%s%s\n",(inf->st_mode&64)?GREEN:WHITE,cur->d_name,WHITE);
+    } 
   }
-  return 0;
+  free(inf);
 }
 
 
 int main(){
+  char name[256]=".";
+  dirinfo(name, 0);
+  /*
+  printf("Looking at: %s\n", filename);
+  char* suffixes[7] = {"bytes","kilobytes","megabytes","gigabytes","terabytes","petabytes","exabytes", "zettabytes", "yottabytes", "brontobytes", "gegobytes"};
+  int logp = log(garbagefile->st_size)/log(1024);
+  printf("\nSize of the file is:%.1f %s\n",garbagefile->st_size/pow(1024,logp),suffixes[logp]);
+  printf("Exact file size (for larger files) is ");
+  sizinga(garbagefile->st_size,suffixes);
+  printf("\n\nLast time of access: %s\n",ctime(&(garbagefile->st_atime)));
+  */
 
-  dirinfo(".");
-  /*printf("Looking at: %s\n", filename);
-    char* suffixes[7] = {"bytes","kilobytes","megabytes","gigabytes","terabytes","petabytes","exabytes", "zettabytes", "yottabytes", "brontobytes", "gegobytes"};
-    int logp = log(garbagefile->st_size)/log(1024);
-    printf("\nSize of the file is:%.1f %s\n",garbagefile->st_size/pow(1024,logp),suffixes[logp]);
-    printf("Exact file size (for larger files) is ");
-    sizinga(garbagefile->st_size,suffixes);
-    printf("\n\nFile permissions: ");
-    char*perms="-xwrxwrxwr";
-    printf("%c",garbagefile->st_mode&S_IFDIR?'d':'-');
-    for(int i=10;--i;)printf("%c",perms[i*(garbagefile->st_mode*2>>i&1)]);
-    printf("\n\nLast time of access: %s\n",ctime(&(garbagefile->st_atime))); */
-    return 0;
+  return 0;
 }
