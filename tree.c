3b72a9458c9473a3dbd63a70a0e128c510009abf
Theodore Peters <tpeters@stuy.edu>
2018-11-01 17:50:38 -0400

    .....

--- tree.c
+++ tree.c
@@ -10,38 +10,50 @@
 #define BLUE "\e[96m"
 #define GREEN "\e[92m"
 #define PERMS "-xwrxwrxwr"
-void sizinga(int i,char**p) {
-  if(i){
-    sizinga(i>>10,p+1);
-    if(i&1023)printf("%d %s ",i&1023,*p);
-  }
+#define PREFIXES "?KMGT"
+
+void sizinga(int s){
+  if (s>=1024) {
+    char logp = log(s)/log(1024);
+    printf("[%.1f%cB]\n",s/pow(1024,logp),PREFIXES[logp]);
+  }else
+    printf("[%dB]\n",s);
 }
-void dirinfo(char*name,int depth){
+void perminga(int p){
+    printf("%c",p&S_IFDIR?'d':'-');
+    for(int i=10;--i;)printf("%c",PERMS[i*(p*2>>i&1)]);
+}
+void treeinga(char d,char e){
+  for(int i=d;i--;)printf("\u2502  ");
+  printf("%s\u2500\u2500 ",e?"\u251C":"\u2514");
+}
+int dirinfo(char*name,int depth){
   DIR*root=opendir(name);
   struct stat*inf=malloc(sizeof(struct stat));
-  int ep=strlen(name);
+  int ep=strlen(name),dirsize=0;
   name[ep++]='/';
   for(struct dirent*cur;cur=readdir(root);){
     if(*cur->d_name=='.')
       continue;
     strcpy(name+ep,cur->d_name);
     stat(name,inf);
+    perminga(inf->st_mode);
+    treeinga(depth,1);
     if(cur->d_type&DT_DIR){
-      printf("d");
-      for(int i=10;--i;)printf("%c",PERMS[i*(inf->st_mode*2>>i&1)]);
-      printf(" +");
-      for(int i=0;i<=depth;i++)printf("--");
-      printf("%s%s/%s\n",BLUE,cur->d_name,WHITE);
-      dirinfo(name, depth+1);
-    } else {
-      printf("-");
-      for(int i=10;--i;)printf("%c",PERMS[i*(inf->st_mode*2>>i&1)]);
-      printf(" +");
-      for(int i=0;i<=depth;i++)printf("--");
-      printf("%s%s%s\n",(inf->st_mode&64)?GREEN:WHITE,cur->d_name,WHITE);
-    } 
+      printf("%s%s%s/\n",BLUE,cur->d_name,WHITE);
+      dirsize+=dirinfo(name,depth+1);
+    }else{
+      printf("%s%s%s%s ",inf->st_mode&64?GREEN:WHITE,cur->d_name,WHITE,inf->st_mode&64?"* ":" ");
+      sizinga(inf->st_size);
+      dirsize+=inf->st_size;
+    }
+    
   }
   free(inf);
+  printf(" dir size:");
+  treeinga(depth,0);
+  sizinga(dirsize);
+  return dirsize;
 }
 
 
@@ -50,9 +62,7 @@ int main(){
   dirinfo(name, 0);
   /*
   printf("Looking at: %s\n", filename);
-  char* suffixes[7] = {"bytes","kilobytes","megabytes","gigabytes","terabytes","petabytes","exabytes", "zettabytes", "yottabytes", "brontobytes", "gegobytes"};
   int logp = log(garbagefile->st_size)/log(1024);
-  printf("\nSize of the file is:%.1f %s\n",garbagefile->st_size/pow(1024,logp),suffixes[logp]);
   printf("Exact file size (for larger files) is ");
   sizinga(garbagefile->st_size,suffixes);
   printf("\n\nLast time of access: %s\n",ctime(&(garbagefile->st_atime)));
