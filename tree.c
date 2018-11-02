d7b2851ae360cb394189c4520292bdbc59c13c0c
anotherLostKitten <tpeters@stuy.edu>
2018-11-02 10:58:04 -0400

    ham sandwich

--- tree.c
+++ tree.c
@@ -5,6 +5,7 @@
 #include <math.h>
 #include <dirent.h>
 #include <string.h>
+#include <errno.h>
 
 #define WHITE "\e[0m"
 #define BLUE "\e[96m"
@@ -15,8 +16,8 @@
 #define PREFIXES "?KMGT"
 
 void sizinga(int s){
-  if (s>=1024) {
-    char logp = log(s)/log(1024);
+  if(s>=1024){
+    char logp=log(s)/log(1024);
     printf("[%s%.1f %cB%s]\n",RED,s/pow(1024,logp),PREFIXES[logp],WHITE);
   }else
     printf("[%s%d B%s]\n",RED,s,WHITE);
@@ -31,14 +32,18 @@ void treeinga(char d,char e){
   printf("%s\u2500\u2500 ",e?"\u251C":"\u2514");
 }
 int dirinfo(char*name,int depth){
-  if(!depth)
-    printf("           %s.%s\n",BLUE,WHITE);
   DIR*root=opendir(name);
+  if(!root){
+    printf("%sERROR!%s %s\n",RED,WHITE,strerror(errno));
+    return 0;
+  }
+  if(!depth)
+    printf("           %s%s%s\n",BLUE,name,WHITE);
   struct stat*inf=malloc(sizeof(struct stat));
   int ep=strlen(name),dirsize=0;
   name[ep++]='/';
   for(struct dirent*cur;cur=readdir(root);){
-    if(!strcmp(cur->d_name,".") || !strcmp(cur->d_name,".."))
+    if(!strcmp(cur->d_name,".")||!strcmp(cur->d_name,".."))
       continue;
     strcpy(name+ep,cur->d_name);
     stat(name,inf);
@@ -54,6 +59,7 @@ int dirinfo(char*name,int depth){
     }
   }
   free(inf);
+  closedir(root);
   printf("          ");
   treeinga(depth,0);
   sizinga(dirsize);
@@ -61,8 +67,15 @@ int dirinfo(char*name,int depth){
 }
 
 
-int main(){
-  char name[256]=".";
-  dirinfo(name, 0);
+int main(int argc,char*argv[]){
+  char name[256];
+  if(argc>1)
+    strcpy(name,argv[1]);
+  else{
+    printf("Enter directory name:\n");
+    scanf("%s",name);
+  }
+  printf("\n%s\n",name);
+  dirinfo(name,0);
   return 0;
 }
